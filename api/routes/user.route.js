fd568fd636b06e85ae82d7c3e2cdb81c3e76527c
Ale <aletiscornia96@gmail.com>
2024-10-02 08:12:17 -0300

    get users api route completa

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { updateUser, deleteUser, signOut } from "../controllers/user.controller.js";
+import { updateUser, deleteUser, signOut, getUsers } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.put('/update/:userId', verifyToken,updateUser);
 router.delete('/delete/:userId', verifyToken,deleteUser);
 router.post('/signout', signOut)
+router.get('/getusers', verifyToken, getUsers)
 
 
 export default router;
