9909dc68ba4d1d7b7799ad740264c73f53d8eb6f
Ale <aletiscornia96@gmail.com>
2024-10-03 13:04:31 -0300

    listado de noticias

--- api/controllers/comments.controller.js
+++ api/controllers/comments.controller.js
@@ -94,3 +94,22 @@ export const deleteComment = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getcomments = async (req, res, next) => {
+    if(!req.user.isAdmin) return next(errorHandler(401, 'No tienes permisos para ver todas las noticias'))
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 0;
+        const comments = await Comment.find()
+            .sort({createdAt: sortDirection})
+            .skip(startIndex)
+            .limit(limit);
+        const totalComments = await Comment.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() -1, now.getDate());
+        const lastMonthComments = await Comment.countDocuments({createdAt: {$gte: oneMonthAgo} });
+        res.status(200).json({ comments, totalComments, lastMonthComments});
+    } catch (error) {
+        next(error)
+    }
+}
