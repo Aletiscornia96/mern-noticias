436da0bb446a6d2199776a34fcaad603c594c397
Ale <aletiscornia96@gmail.com>
2024-10-04 01:21:51 -0300

    dashboard agregado al sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import  DashProfile  from '../components/DashProfile';
 import  DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers'
 import DashComments from '../components/DashCommets';
+import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -31,6 +32,8 @@ export default function Dashboard() {
       {tab === 'users' && <DashUsers/>}
       {/* {commments} */}
       {tab === 'comments' && <DashComments/>}
+      {/* {dashboard} */}
+      {tab === 'dash' && <DashboardComp/>}
     </div>
   );
 }
